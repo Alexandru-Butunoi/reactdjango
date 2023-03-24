808b376e79916530c04e3e085b2fa4f604fe4ee9
zulu607i <andreimmircea1@gmail.com>
2023-03-24 18:24:14 +0200

    added realtors app (tested the endpoints with Postman)

---
+++ backend/realtors/views.py
@@ -0,0 +1,23 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import permissions
+from .models import Realtor
+from .serializeres import RealtorSerializer
+
+
+class RealtorListView(ListAPIView):
+    permission_classes = (permissions.AllowAny, )
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+    pagination_class = None
+
+
+class RealtorView(RetrieveAPIView):
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+
+
+class TopSellerView(ListAPIView):
+    queryset = Realtor.objects.filter(top_seller=True)
+    permission_classes = (permissions.AllowAny, )
+    serializer_class = RealtorSerializer
+    pagination_class = None
